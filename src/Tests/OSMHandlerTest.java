5855fe74685eb4f5f36955f48d4c18ceeb6e344b
Anders <aedl@itu.dk>
2015-03-23 15:12:03 +0100

    Tilføjet pom.xml til brug af JUnit

---
+++ src/Tests/OSMHandlerTest.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import org.junit.Test;
+
+/**
+ * Created by Anders on 23-03-2015.
+ */
+public class OSMHandlerTest {
+
+    @Test
+    public void testSomething() {
+        //test a case
+    }
+
+
+
+}
