ad0665e6739fae3eefcb8cf127bcb0cbf19bb5d3
Kevin <kejj@itu.dk>
2015-04-09 19:02:42 +0200

    OSMHandler test

--- src/Tests/OSMHandlerTest.java
+++ src/Tests/OSMHandlerTest.java
@@ -12,9 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 import sun.tools.jar.Main;
 
-import java.awt.geom.Path2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
+import java.awt.geom.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -25,6 +23,7 @@ import java.util.List;
  * Test class for the OSMHandler class
  */
 public class OSMHandlerTest {
+    private static final double DELTA = 1e-15;
     Model m = Model.getModel();
     InputStream inputStream;
 
@@ -50,7 +49,7 @@ public class OSMHandlerTest {
         wayCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6687563), 12.5817373f));
         wayCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6685247), 12.5823799f));
         Path2D way1 = PathCreater.createWay(wayCoords);
-        MapFeature highway = new Highway(way1, 13, "teriary", false);
+        MapFeature highway = new Highway(way1, 13, "tertiary", false);
         highway.setValueName(ValueName.TERTIARY);
 
         ArrayList<List<MapData>> queryList = m.getVisibleData(new Rectangle2D.Float(0, 0, 500, 500));
@@ -70,11 +69,25 @@ public class OSMHandlerTest {
 
         }
 
-        for (Point2D wayCoord : wayCoords) {
-            if (mapFeature != null) {
-                Assert.assertTrue(mapFeature.getWay().contains(wayCoord));
+        if (mapFeature != null) {
+            PathIterator pI = mapFeature.getShape().getPathIterator(new AffineTransform());
+            int i = 0;
+            while(!pI.isDone()){
+                float[] points = new float[6];
+
+                pI.currentSegment(points);
+
+
+                Assert.assertEquals(points[0], wayCoords.get(i).getY(),DELTA);
+                Assert.assertEquals(points[1], wayCoords.get(i).getX(), DELTA);
+
+                pI.next();
+                i++;
+
             }
+
         }
+
         if (mapFeature != null) {
             Assert.assertEquals(highway.toString(), mapFeature.toString());
         }
