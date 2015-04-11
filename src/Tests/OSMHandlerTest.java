ef6cd96719222efcb78715c54f3c7a75bbece0b2
Anders <aedl@itu.dk>
2015-04-11 13:09:07 +0200

    OSMHandlertest continued

--- src/Tests/OSMHandlerTest.java
+++ src/Tests/OSMHandlerTest.java
@@ -1,12 +1,11 @@
 package Tests;
 
+import MapFeatures.Barrier;
 import MapFeatures.Highway;
+import MapFeatures.Leisure;
 import Model.Model;
 import Model.MapData;
-import Model.MapFeature;
-import Model.PathCreater;
 import Model.MapCalculator;
-import Model.ValueName;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,6 +25,7 @@ public class OSMHandlerTest {
     private static final double DELTA = 1e-15;
     Model m = Model.getModel();
     InputStream inputStream;
+    List<MapData> streetList;
 
     /**
      * Sets up an osm test file, which enables us to cover all the functions the OSMHandler invokes
@@ -38,56 +38,57 @@ public class OSMHandlerTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        streetList = m.getVisibleStreets(new Rectangle2D.Float(0, 0, 500, 500));
     }
 
     @Test
     public void MapFeatureWayTest() {
-        List<Point2D> wayCoords = new ArrayList<>();
-        wayCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6695228), 12.5802146f));
-        wayCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6693366), 12.5805524f));
-        wayCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6690488), 12.5811529f));
-        wayCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6687563), 12.5817373f));
-        wayCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6685247), 12.5823799f));
-        Path2D way1 = PathCreater.createWay(wayCoords); //first way
+        List<Point2D> highwayCoords = new ArrayList<>();
+        highwayCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6695228), 12.5802146f));
+        highwayCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6693366), 12.5805524f));
+        List<Point2D> barrierCoords = new ArrayList<>();
+        barrierCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6677049), 12.5853856f));
+        barrierCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6676617), 12.5853626f));
+        List<Point2D> leisureCoords = new ArrayList<>();
+        leisureCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6653196), 12.5818835f));
+        leisureCoords.add(new Point2D.Float((float) MapCalculator.latToY(55.6654239), 12.5820209f));
 
-        List<MapData> queryList = m.getVisibleStreets(new Rectangle2D.Float(0, 0, 500, 500));
+        //The map features we want
+        Highway highway = null;
+        Barrier barrier = null;
+        Leisure leisure = null;
 
-        List<MapData> mapDataList = null;
-        mapDataList = queryList; //throw all map features and icons into one list
 
-
-        Highway highway_tertiary = null; //The map feature we want
-
-        assert mapDataList != null;
-        for (int i = 0 ; i < mapDataList.size() ; i++) {
-            MapData mapData = mapDataList.get(i);
-            if (mapData instanceof Highway && ((Highway) mapData).getValue().equals("tertiary")) {
-                highway_tertiary = (Highway) mapData;
-                
-            }
+        assert streetList != null;
+        for (MapData mapData : streetList) {
+            if (mapData instanceof Highway && highway.getValue().equals("tertiary"))
+                highway = (Highway) mapData;
+            else if (mapData instanceof Barrier && barrier.getValue().equals("hedge"))
+                barrier = (Barrier) mapData;
+            else if (mapData instanceof Leisure && leisure.getValue().equals("garden"))
+                leisure = (Leisure) mapData;
         }
 
-
-
-        if (highway_tertiary != null) {
-            PathIterator pI = highway_tertiary.getShape().getPathIterator(new AffineTransform());
-            int i = 0;
-            while(!pI.isDone()){
-                float[] points = new float[6];
-
-                pI.currentSegment(points);
-
-
-                Assert.assertEquals(points[0], wayCoords.get(i).getY(), DELTA);
-                Assert.assertEquals(points[1], wayCoords.get(i).getX(), DELTA);
-
-                pI.next();
-                i++;
-
-            }
-
+        if(highway != null && barrier != null && leisure != null) {
+            PathIterator highwayPath = highway.getShape().getPathIterator(new AffineTransform()); //object that iterates along a shape
+            PathIterator barrierPath = barrier.getShape().getPathIterator(new AffineTransform());
+            PathIterator leisurePath = leisure.getShape().getPathIterator(new AffineTransform());
+            pathIterate(highwayPath, highwayCoords);
+            pathIterate(barrierPath, barrierCoords);
+            pathIterate(leisurePath, leisureCoords);
         }
+    }
 
+    private void pathIterate(PathIterator path, List<Point2D> wayCoords) {
+        int i = 0;
+        while(!path.isDone()) {
+            float[] points = new float[6];
+            path.currentSegment(points);
+            Assert.assertEquals(points[0], wayCoords.get(i).getY(), DELTA); //point[0] is y-coordinate
+            Assert.assertEquals(points[1], wayCoords.get(i).getX(), DELTA); //point[1] is x-coordinate
+            path.next();
+            i++;
+        }
     }
 
 }
