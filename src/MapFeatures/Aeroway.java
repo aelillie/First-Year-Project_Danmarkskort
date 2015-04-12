a90a05be10e2a7ebac18d12d3e4c67c3e3855b38
Kevin <kejj@itu.dk>
2015-04-12 14:19:31 +0200

    test

--- src/MapFeatures/Aeroway.java
+++ src/MapFeatures/Aeroway.java
@@ -7,7 +7,7 @@ import java.awt.geom.Path2D;
 
 /**
  * Created by Anders on 11-03-2015.
- */
+*/
 public class Aeroway extends MapFeature {
 
     public Aeroway(Path2D way, int layer_value, String value) {
